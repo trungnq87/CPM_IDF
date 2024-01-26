28f8ffacc16e5b5887a5f3dd2e218353edfef1ed
Trung Nguyen Quang <54671440+trungnq87@users.noreply.github.com>
2024-01-26 13:16:49 -0500

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,8 +1,8 @@
 # ================================================================= #
 #
-# Update : Wed Aug  9 11:34:44 EDT 2023
+# Update : Wed Jan 10 10:46:58 EST 2024
 #
-# Every time, start with Setup.sh.
+# Every time, start with . ./Setup.sh.
 # This script just creates folders for your study.
 #
 # Although this is not recommended for HPC server
@@ -17,31 +17,56 @@
 #
 # What to MODIFY (or not)
 #
-workdir=`pwd`
+workdir=`pwd`    # Your working directory
+version='011224' # Version of codes
+dev=false        # true # If you are in development step (not recommend)
 #
 # ================================================================= #
+#
 # What NOT to touch :
 #
 # Make "basic" directories
 cd $workdir
 mkdir -p Input    # All your input
 mkdir -p Output   # All your output
-mkdir -p Src      # All your source code
 mkdir -p Tmp      # Temporary working files (e.g. submitted jobs)
                   # Basically, you can delete "Tmp" anytime, if needed 
 #
 # Make folders that you know you need for processing IDF curves
-mkdir -p Src/00_Pre_Process
-mkdir -p Src/01_Bias_Correction
-mkdir -p Src/02_Generate_IDF
-mkdir -p Src/03_Analysis
+# For Github version, this should be replaced by tar -xzvf Src.tar.gz
+#
+if [ "$dev" = true ] ; then 
+    mkdir -p Src      # All your source code
+    mkdir -p Src/00_Pre_Process
+    mkdir -p Src/01_Bias_Correction
+    mkdir -p Src/02_Generate_IDF
+    mkdir -p Src/03_Analysis
+fi
+#
 # Note: I try to keep each layer as simple as it can be !
 #       e.g. no more than 03 tasks for each layer
 #
 echo "The folders that you have now :"
 ls
-ls Src/* 
+ls Src/*
+#
+# Occam's razor :) Check the version that you are working on.
+#
+export IDF_VERSION=$version
+#
+if [[ -n "$IDF_VERSION" ]]; then
+    echo "You are using the CPM_IDF version : $IDF_VERSION"
+else
+    echo "You should export IDF_VERSION :) or things will mess up."
+fi
+#
+# Fix your working directory
+#
+export wrkdir=${workdir}/ # Remember / or no need ?
+echo "Your working directory is : $wrkdir (!)"
 #
 # ================================================================= #
+#
 # Fin ! 
+#
 # ================================================================= #
